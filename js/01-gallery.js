942dd7c2d13590a0b40d72ce3c6bea9cdf304f4a
apazient <nataliia.amanzhayeva@web.de>
2023-01-26 11:33:07 +0100

    update hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+const galleryMarktUp = createGalleryCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML(`beforeend`, galleryMarktUp);
+galleryContainer.addEventListener(`click`, onImgClick);
+
+
+function createGalleryCard(galleryItems) {
+return galleryItems.map(({preview,original,description})=> {
+    return     ` <div class="gallery__item">
+   <a class="gallery__link" href="${preview}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`
+    }).join(``);     
+}
+
+
+function onImgClick(event) {
+    if (event.target.nodeName !== `IMG`) {
+        return;
+    }
+    
+    //cancels img download
+    event.preventDefault();     
+   
+     //create modal popup
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="1280">`);
+    instance.show()
+}
+  
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+const galleryMarktUp = createGalleryCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML(`beforeend`, galleryMarktUp);
+galleryContainer.addEventListener(`mousedown`, onImgClick);
+
+
+function createGalleryCard(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image"   src="${preview}"  alt="${description}" />
+</a>`;
+  }).join(``);
+}
+
+function onImgClick(event) {
+     if (event.target.nodeName !== `IMG`) {
+        return;
+    }
+    
+    //cancels img download
+    event.preventDefault(); 
+
+  new SimpleLightbox('.gallery a', {
+        captions: true,   
+        captionsData: `alt`,
+        captionPosition: 'bottom',
+        captionDelay: 250,
+   
+    });
+ 
+}
